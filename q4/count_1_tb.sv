47fd42c70de16736445757354add43cb5d523625
pochacco0417 <itzy1055900172@gmail.com>
2025-09-14 00:14:42 +0200

    Update count_1_tb.sv

--- q4/count_1_tb.sv
+++ q4/count_1_tb.sv
@@ -6,5 +6,22 @@ module count_1_tb;
   // Outputs
   logic [2:0] out;
 
-  // complete
+  count_1 dut(
+    .a(a),
+    .out(out)
+  );
+
+  initial begin
+    a=4'b0000;
+    #10ns;
+    a=4'b0001;
+    #10ns;
+    a=4'b0101;
+    #10ns;
+    a=4'b1110;
+    #10ns;
+    a=4'b1111;
+    #10ns;
+    $finish
+  end
 endmodule

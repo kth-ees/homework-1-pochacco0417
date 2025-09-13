dec5f58aba72aecde214b1d8ba08466423ea3ca4
pochacco0417 <itzy1055900172@gmail.com>
2025-09-14 00:46:30 +0200

    Update bin2gray.sv

--- q5/bin2gray.sv
+++ q5/bin2gray.sv
@@ -2,7 +2,10 @@ module bin2gray (
   input logic [3:0] binary,
   output logic [3:0] gray
 );
-  // …
-  // Add your description here
-  // …
+  
+  assign gray[3]=binary[3];
+  assign gray[0]=binary[0] xor binary[1];
+  assign gray[1]=binary[1] xor binary[2];
+  assign gray[2]=binary[2] xor binary[3];
+  
 endmodule

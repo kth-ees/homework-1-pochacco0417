eff8300014ea2ba1e651927104e93c1ec0f4726b
pochacco0417 <itzy1055900172@gmail.com>
2025-09-13 23:42:45 +0200

    Update count_1.sv

--- q4/count_1.sv
+++ q4/count_1.sv
@@ -2,7 +2,11 @@ module count_1 (
   input logic [3:0] a,
   output logic [2:0] out
 );
-  // …
-  // Add your description here
-  // …
+  always_comb begin
+    out=3'b000;
+    for(int i=0; i<4; i++) begin
+      if(a[i]==1'b1)
+        out=out+1;
+    end
+  end
 endmodule

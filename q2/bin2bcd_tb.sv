995d158990d2576b9620a50af4f828fe4bebd2da
pochacco0417 <itzy1055900172@gmail.com>
2025-09-13 12:28:15 +0200

    Update bin2bcd_tb.sv

--- q2/bin2bcd_tb.sv
+++ q2/bin2bcd_tb.sv
@@ -5,5 +5,18 @@ module bin2bcd_tb;
   logic [3:0] bcd;
   logic carry;
 
-  // complete your tb
+  bin2bcd dut(
+    .binary(binary),
+    .bcd(bcd),
+    .carry(carry)
+  );
+
+  initial begin
+    for(int i=0; i<16; i++) begin
+      binary=i;
+      #10
+    end
+    $stop;
+  end
+    
 endmodule

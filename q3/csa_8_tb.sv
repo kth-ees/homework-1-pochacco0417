409f015436e7f47c1440c93221c54b5e3e3731f9
pochacco0417 <itzy1055900172@gmail.com>
2025-09-13 18:09:23 +0200

    Update csa_8_tb.sv

--- q3/csa_8_tb.sv
+++ q3/csa_8_tb.sv
@@ -6,6 +6,22 @@ module csa_8_tb;
   logic [7:0] sum,
   logic carry;
 
-  // complete
+  csa_8 dut(
+    .a(a),
+    .b(b),
+    .sum(sum),
+    .carry(carry)
+  );
+
+  initial begin
+    $display("a\t b\t carry\t sum");
+    for(int i=0; i<10; i++) begin
+      a=$urandom_range(0, 255);
+      b=$urandom_range(0, 255);
+      #10
+      $display("%d\t%d\t%b\t%d", a, b, carry, sum);
+    end
+    $finish
+  end
   
 endmodule

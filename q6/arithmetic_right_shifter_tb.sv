45f1aaed87c26072191f3eb2747319c509909a72
pochacco0417 <itzy1055900172@gmail.com>
2025-09-14 13:14:13 +0200

    Update arithmetic_right_shifter_tb.sv

--- q6/arithmetic_right_shifter_tb.sv
+++ q6/arithmetic_right_shifter_tb.sv
@@ -2,6 +2,26 @@ module arithmetic_right_shifter_tb;
 
   localparam N = 8;
 
-  // complete
-  // Inputs
+  logic [N-1:0] input_data;
+  logic [1:0] control;
+  logic [N-1:0] shifed_result;
+
+  arithmetic_right_shifter #(.N(N)) dut(
+    .input_data(input_data),
+    .control(control),
+    .shifted_result(shifted_result)
+  );
+
+  initial begin
+    input_data=8'b10000000;
+    control=2'b00;
+    #10;
+    control=2'b01;
+    #10;
+    control=2'b10;
+    #10;
+    control=2'b11;
+    #10;
+    $stop;
+  end
 endmodule
